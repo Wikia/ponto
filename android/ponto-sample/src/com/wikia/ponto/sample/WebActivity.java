61e26a49653a774581ac8a3c3c117b25175f92e7
ArturKlajnerok <a.klajnerok@gmail.com>
2014-06-19 11:12:04 -0700

    Created ponto android sample project

---
+++ android/ponto-sample/src/com/wikia/ponto/sample/WebActivity.java
@@ -0,0 +1,30 @@
+package com.wikia.ponto.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WebActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.web, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
